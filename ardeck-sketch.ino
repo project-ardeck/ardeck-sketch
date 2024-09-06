21a3ba04da75da45c84c72549f310268cc75dfc9
sekurosu-daimajin <134798705+sekurosu-daimajin@users.noreply.github.com>
2024-09-06 14:19:38 +0900

    Update ardeck-sketch.ino

--- ardeck-sketch.ino
+++ ardeck-sketch.ino
@@ -1,3 +1,22 @@
+// ardeck-sketch.ino  This sketch makes the arduino a stream deck. By using button/potentiometer etc... you can control about your pc(example:Volume control,shortcut).
+// Copyright (C) 2024 project-ardeck 
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+
 #define VALUEOFDIGITAL 8                                    //Number of buttons -1 Don't change too much.
 #define VALUEOFANALOG 6
 int button_stat[VALUEOFDIGITAL + 1][4] = {
