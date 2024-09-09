33b635df0bbab98bdce4d730ea6de6f4dd8f214d
akurakuu <77902040+akurakuu@users.noreply.github.com>
2024-09-09 12:12:42 +0900

    akita

--- config.h
+++ config.h
@@ -4,5 +4,13 @@
 #define DIGITAL_SWHTCH 0 // Number of digital switches
 #define ANALOG_SWITCH 0 // Number of digital switches
 
+int DIGITAL_SWHTCH_ID[DIGITAL_SWHTCH] = {
+    // 2, 3, 4, 5, 6, 7
+};
+
+int ANALOG_SWITCH_ID[ANALOG_SWITCH] = {
+    // 0, 1
+}
+
 
 #endif
