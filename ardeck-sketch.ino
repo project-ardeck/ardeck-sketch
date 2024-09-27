caee0d904b5e29665bf445a4e4d4009afbcc8c2b
sekurosu-daimajin <134798705+sekurosu-daimajin@users.noreply.github.com>
2024-09-27 13:14:12 +0900

    Update ardeck-sketch.ino

--- ardeck-sketch.ino
+++ ardeck-sketch.ino
@@ -17,28 +17,6 @@
 
 #include "config.h"
 
-// #define VALUEOFDIGITAL 8                                    //Number of buttons -1 Don't change too much.
-// #define VALUEOFANALOG 6
-// int button_stat[VALUEOFDIGITAL + 1][4] = {
-//   { 2,1,0,0},						                                   //button ID (pin number), button use or not, button ON/OFF assignment, binary number to send
-//   { 3,1,0,0},                                              //Example.{2,1,0,0}->Use pin-2, button available, button OFF, button status preservation.
-//   { 4,1,0,0},.
-//   { 5,1,0,0},
-//   { 6,1,0,0},
-//   { 7,1,0,0},
-//   { 8,1,0,0},
-//   { 9,1,0,0},
-//   {10,1,0,0},
-// }; 
-
-// int analog_stat[VALUEOFANALOG][4]={          // pin number, pin use or not, pin value (voltage) assignment, binary number to send
-//   {A0,1,0,0},
-//   {A1,1,0,0},
-//   {A2,1,0,0},
-//   {A3,1,0,0},
-//   {A4,1,0,0},
-//   {A5,1,0,0},
-// };
 
 
 char identifier[4]={'A', 'D', 'E', 'C'};                     //Identifier. Sent at each Serial.write.
@@ -70,7 +48,7 @@ void loop() {
 
   for(int i=0;i<=VALUEOFDIGITAL;i++){
     button_stat[i][3]=B00000010 + (i << 1);
-    if(button_stat[i][2] == switchmode){                              //Normal open/close switching (open:1, close:0)
+    if(button_stat[i][2] == switchmode){                              //Normal open/close switching.
       button_stat[i][3] = button_stat[i][3] + 1 ;
     }
     if(button_stat[i][1] == 1){ 
