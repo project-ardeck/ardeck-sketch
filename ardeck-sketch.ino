b986902f23f50bea4c47efdc2e5e8fbcc2cb521c
akurakuu <akurakuu0725@gmail.com>
2025-04-07 20:16:37 +0900

    アナログスイッチのCOBSエンコード処理追加

--- ardeck-sketch.ino
+++ ardeck-sketch.ino
@@ -104,19 +104,30 @@ int send_a(int pin, int state)
     return -1;
   }
 
-  byte body[3] = {0, 0, 0};
-  body[0] =
+  byte body[5] = {0x04, 0xFF, 0xFF, 0xFF, 0}; // 0:COBS_HEAD, 1:DATA, 2:DATA, 3:CHECKSUM, 4:COBS_END
+  body[1] =
       (1 << 7) |
       ((pin & 0b00011111) << 2) |
       (state & 0b1100000000) >> 8;
 
-  body[1] = (byte)state;
+  body[2] = (byte)state;
 
-  body[2] = check_sum(body, 2);
+  body[3] = check_sum(&body[1], 2);
 
-  Serial.write(IDENTIFIER, 2);
+  // COBS encode
+  int prev_zero_index = 4;
+  for (int i = 4; i >= 0; i--)
+  {
+    if (body[i] == 0)
+    {
+      body[i] = prev_zero_index - i + 1;
+      prev_zero_index = i;
+    }
+  }
+
+  // TODO: その他の場合
+
   Serial.write(body, 3);
-  Serial.write(&IDENTIFIER[2], 2);
 
   return 0;
 }
