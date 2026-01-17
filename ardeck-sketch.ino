dfb3e94c8449ba93385f441a14cabbb7503f739c
akurakuu <kuuakura@gmail.com>
2026-01-17 21:41:41 +0900

    スイッチの状態が変わった時だけ送信されるように変更

--- ardeck-sketch.ino
+++ ardeck-sketch.ino
@@ -17,12 +17,45 @@
 
 #include "config.h"
 
+/// @brief ボディーデータの開始インデックスと長さを渡すと、その合計値を1バイトで返します。
+/// @param data 配列のポインタ
+/// @param length 長さ
+/// @return １バイトの合計値
+byte check_sum(byte *data, int length)
+{
+  byte sum = 0;
+  for (int i = 0; i < length; i++)
+  {
+    sum += data[i];
+  }
+  return sum;
+}
+
+/// @brief スイッチの状態を保持する構造体
+struct SwitchState {
+  /// @brief ピン番号
+  int pin;
+  /// @brief スイッチの状態を表す数値
+  int state;
+};
+
+// 1ループ前のデジタルスイッチの状態
+SwitchState prev_d_state[NUMBER_OF_D_SWITCH];
+SwitchState prev_a_state[NUMBER_OF_A_SWITCH];
+
 void setup()
 {
   // Init of digital pin
   for (int i = 0; i < NUMBER_OF_D_SWITCH; i++)
   {
+    prev_d_state[i] = SwitchState {d_switch_pin[i][0], 0};
     pinMode(d_switch_pin[i][0], INPUT_PULLUP);
+  }
+
+  // Init of analog pin
+  for (int i = 0; i < NUMBER_OF_A_SWITCH; i++)
+  {
+    prev_a_state[i] = SwitchState {a_switch_pin[i], 0};
   }
 
   Serial.begin(BAUD_RATE);
@@ -32,22 +65,48 @@ void loop()
 {
   for (int i = 0; i < NUMBER_OF_D_SWITCH; i++)
   {
+    int pin = d_switch_pin[i][0];
     int state = 0;
-    state = digitalRead(d_switch_pin[i][0]);
+    state = digitalRead(pin);
     if (d_switch_pin[i][1] == 0)
     {
       state ^= 1;
     }
-    send_d(d_switch_pin[i][0], state);
+
+    // 前回の情報と比較して、スイッチ状態が変わっていれば送信する。
+    for (int j = 0; j < NUMBER_OF_D_SWITCH; i++)
+    {
+      if (prev_d_state->pin == pin)
+      {
+        if (prev_d_state->state != state)
+        {
+          send_d(pin, state);
+        }
+        prev_d_state->state = state;
+      }
+    }
 
     delay(SEND_INTERVAL);
   }
 
   for (int i = 0; i < NUMBER_OF_A_SWITCH; i++)
   {
+    int pin = a_switch_pin[i];
     int state = 0;
     state = 1023 - analogRead(a_switch_pin[i]); // if you delete "1023", analog input will invert
-    send_a(a_switch_pin[i], state);
+
+    // 前回の情報と比較して、スイッチ状態が変わっていれば送信する。
+    for (int j = 0; j < NUMBER_OF_A_SWITCH; i++)
+    {
+      if (prev_a_state->pin == pin)
+      {
+        if (prev_a_state->state != state)
+        {
+          send_a(pin, state);
+        }
+        prev_a_state->state = state;
+      }
+    }
 
     delay(SEND_INTERVAL);
   }
@@ -60,11 +119,10 @@ int send_d(int pin, int state)
     return -1;
   }
 
-  byte body[4] = {0x03, 0xFF, 0xFF, 0}; // 0:COBS_HEAD, 1:DATA, 2:CHECKSUM, 3:COBS_END
+  byte body[3] = {0x02, 0xFF, 0x00}; // 0:COBS_HEAD, 1:DATA, 2:COBS_END
   body[1] =
       (((pin & 0b00111111) << 1) |
        (state & 1));
-  body[2] = check_sum(&body[1], 1);
 
   // COBS encode
   if (body[1] == 0 && body[2] == 0)
@@ -76,13 +134,6 @@ int send_d(int pin, int state)
   // DATAが1バイトであるので、どちらかのみが0になるという状態はありえないため、記述を省略
 
   Serial.write(body, 4);
-
-  // for (int i = 0; i < 4; i++)
-  // {
-  //   Serial.print(body[i], HEX);
-  //   Serial.print(" ");
-  // }
-  // Serial.println();
 
   return 0;
 }
@@ -116,13 +167,6 @@ int send_a(int pin, int state)
   }
 
   Serial.write(body, 5);
-
-  // for (int i = 0; i < 5; i++)
-  // {
-  //   Serial.print(body[i], HEX);
-  //   Serial.print(" ");
-  // }
-  // Serial.println();
 
   return 0;
 }
